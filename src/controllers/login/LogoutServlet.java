d541a618343eb37dd029e7254df4639cd65714c6
kenji.takada <falcon9628@yahoo.co.jp>
2020-12-05 07:22:38 +0900

    Add login/logout link

--- src/controllers/login/LogoutServlet.java
+++ src/controllers/login/LogoutServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/Logout")
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -20,7 +20,6 @@ public class LogoutServlet extends HttpServlet {
      */
     public LogoutServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -31,7 +30,6 @@ public class LogoutServlet extends HttpServlet {
 
         request.getSession().setAttribute("flush", "ログアウトしました。");
         response.sendRedirect(request.getContextPath() + "/login");
-     }
-    //    doGet
+    }
 
 }
